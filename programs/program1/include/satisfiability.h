49aa655d0d145db833fbec13fae290bc61f93fc7
Austin Gill <notgnoshi@gmail.com>
2018-09-24 19:56:56 -0600

    Implement the first circuit

--- programs/program1/include/satisfiability.h
+++ programs/program1/include/satisfiability.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 /**
  * @brief Get individual bits from a number
@@ -13,18 +14,22 @@
 #define EXTRACT_BIT( n, i ) ( ( n & ( 0x1 << i ) ) ? 0x1 : 0x0 )
 
 /**
- * @brief
+ * @brief Checks the given input against the first circuit for satisfiability.
  *
- * @param pid
- * @param z
+ * @param pid The process ID of the calling thread.
+ * @param z The input to check.
+ *
+ * @returns true if the circuit is satisfied. False otherwise.
  */
 bool circuit_one( const uint32_t pid, const uint16_t z );
 
 /**
- * @brief
+ * @brief Checks the given input against the second circuit for satisfiability.
  *
- * @param pid
- * @param z
+ * @param pid The process ID of the calling thread.
+ * @param z The input to check.
+ *
+ * @returns true if the circuit is satisfied. False otherwise.
  */
 bool circuit_two( const uint32_t pid, const uint16_t z );
 
--- programs/program1/src/satisfiability.c
+++ programs/program1/src/satisfiability.c
@@ -2,8 +2,25 @@
 
 bool circuit_one( const uint32_t pid, const uint16_t z )
 {
-    //! @todo Implement.
-    return pid + z;
+    bool bits[16] = { 0 };
+    extract_bits( bits, z );
+
+    // For whatever reason, astyle indents the line continuation too far...
+    if( ( bits[0]  ||  bits[1]  ) && ( !bits[1]  || !bits[3]  ) && ( bits[2]  ||  bits[3]  ) &&
+            ( !bits[3]  || !bits[4]  ) && (  bits[4]  || !bits[5]  ) && ( bits[5]  || !bits[6]  ) &&
+            (  bits[5]  ||  bits[6]  ) && (  bits[6]  || !bits[15] ) && ( bits[7]  || !bits[8]  ) &&
+            ( !bits[7]  || !bits[13] ) && (  bits[8]  ||  bits[9]  ) && ( bits[8]  || !bits[9]  ) &&
+            ( !bits[9]  || !bits[10] ) && (  bits[9]  ||  bits[11] ) && ( bits[10] ||  bits[11] ) &&
+            (  bits[12] ||  bits[13] ) && (  bits[13] || !bits[14] ) && ( bits[14] ||  bits[15] ) )
+    {
+        printf( "%d) %d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", pid,
+                bits[0], bits[1], bits[2], bits[3], bits[4], bits[5], bits[6], bits[7],
+                bits[8], bits[9], bits[10], bits[11], bits[12], bits[13], bits[14], bits[15] );
+        fflush ( stdout );
+        return true;
+    }
+
+    return false;
 }
 
 bool circuit_two( const uint32_t pid, const uint16_t z )
