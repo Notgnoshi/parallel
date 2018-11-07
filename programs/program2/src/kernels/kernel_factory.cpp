1b8bea28c8e6a8b37f262ccb8a66b2270256bb30
Austin Gill <notgnoshi@gmail.com>
2018-11-06 19:33:32 -0700

    Add the CUDA multiplication kernel to the kernel factory

--- programs/program2/src/kernels/kernel_factory.cpp
+++ programs/program2/src/kernels/kernel_factory.cpp
@@ -2,6 +2,7 @@
 #include "kernels/cpu_add.h"
 #include "kernels/cpu_mult.h"
 #include "kernels/cuda_add.h"
+#include "kernels/cuda_mult.h"
 
 KernelFactory::KernelFactory( Operation_e op, Kernel_e kernel ) :
     operation( op ),
@@ -16,18 +17,21 @@ std::shared_ptr<Kernel> KernelFactory::GetKernel()
     case OPERATION_VECTOR_MULTIPLICATION:
         switch( this->kernel )
         {
+        //! @todo Move the default multiplication kernel to CUDA once it's implemented.
         case KERNEL_DEFAULT:
-        case KERNEL_CUDA:
         case KERNEL_CPU:
             return std::make_shared<CpuMultiplicationKernel>();
+
+        case KERNEL_CUDA:
+            return std::make_shared<CudaMultiplicationKernel>();
         }
     case OPERATION_ADDITION:
         switch( this->kernel )
         {
-        case KERNEL_DEFAULT:
         case KERNEL_CPU:
             return std::make_shared<CpuAdditionKernel>();
 
+        case KERNEL_DEFAULT:
         case KERNEL_CUDA:
             return std::make_shared<CudaAdditionKernel>();
         }
