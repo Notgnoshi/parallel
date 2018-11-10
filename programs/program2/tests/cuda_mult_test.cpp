93f6e1e3765a5dff2e0eab5700e9ab9a6b0f093b
Austin Gill <notgnoshi@gmail.com>
2018-11-10 15:06:20 -0700

    Add tests for matrices oddly divisible by square blocks

--- programs/program2/tests/cuda_mult_test.cpp
+++ programs/program2/tests/cuda_mult_test.cpp
@@ -50,50 +50,44 @@ void CudaMultiplicationKernelTest::SimpleSquare()
 
 void CudaMultiplicationKernelTest::MismatchedSmaller()
 {
-    // Matrix_t lhs( 15, 15 );
-    // Matrix_t rhs( 15, 1 );
+    Matrix_t lhs( 15, 15 );
+    Matrix_t rhs( 15, 15 );
+    Matrix_t expected( 15, 15 );
 
-    // for( size_t i = 0; i < lhs.elements; ++i )
-    // {
-    //     lhs.data[i] = static_cast<double>( i );
-    // }
+    for( size_t i = 0; i < lhs.elements; ++i )
+    {
+        lhs.data[i] = 1;
+        rhs.data[i] = 1;
+        expected.data[i] = 15;
+    }
 
-    // for( size_t i = 0; i < rhs.elements; ++i )
-    // {
-    //     rhs.data[i] = static_cast<double>( i ) + 1.5;
-    // }
+    auto kernel = KernelFactory( OPERATION_VECTOR_MULTIPLICATION, KERNEL_CUDA ).GetKernel();
+    auto result = kernel->Operation( lhs, rhs );
 
-    // auto cpu_kernel = KernelFactory( OPERATION_VECTOR_MULTIPLICATION, KERNEL_CPU ).GetKernel();
-    // auto expected = cpu_kernel->Operation( lhs, rhs );
+    result->Print();
 
-    // auto kernel = KernelFactory( OPERATION_VECTOR_MULTIPLICATION, KERNEL_CUDA ).GetKernel();
-    // auto result = kernel->Operation( lhs, rhs );
-
-    // CPPUNIT_ASSERT( *result == *expected );
+    CPPUNIT_ASSERT( *result == expected );
 }
 
 void CudaMultiplicationKernelTest::MismatchedBigger()
 {
-    // Matrix_t lhs( 17, 17 );
-    // Matrix_t rhs( 17, 1 );
+    Matrix_t lhs( 17, 17 );
+    Matrix_t rhs( 17, 17 );
+    Matrix_t expected( 17, 17 );
 
-    // for( size_t i = 0; i < lhs.elements; ++i )
-    // {
-    //     lhs.data[i] = static_cast<double>( i );
-    // }
+    for( size_t i = 0; i < lhs.elements; ++i )
+    {
+        lhs.data[i] = 1;
+        rhs.data[i] = 1;
+        expected.data[i] = 17;
+    }
 
-    // for( size_t i = 0; i < rhs.elements; ++i )
-    // {
-    //     rhs.data[i] = static_cast<double>( i ) + 1.5;
-    // }
+    auto kernel = KernelFactory( OPERATION_VECTOR_MULTIPLICATION, KERNEL_CUDA ).GetKernel();
+    auto result = kernel->Operation( lhs, rhs );
 
-    // auto cpu_kernel = KernelFactory( OPERATION_VECTOR_MULTIPLICATION, KERNEL_CPU ).GetKernel();
-    // auto expected = cpu_kernel->Operation( lhs, rhs );
+    result->Print();
 
-    // auto kernel = KernelFactory( OPERATION_VECTOR_MULTIPLICATION, KERNEL_CUDA ).GetKernel();
-    // auto result = kernel->Operation( lhs, rhs );
-
-    // CPPUNIT_ASSERT( *result == *expected );
+    CPPUNIT_ASSERT( *result == expected );
 }
 
 void CudaMultiplicationKernelTest::SimpleLarge()
